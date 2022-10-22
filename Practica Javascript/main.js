94bd8e6b1bff3222452587dbb778de37c45481df
Randy <randyluna93@gmail.com>
2022-10-21 18:31:03 -0600

    Carpeta main.js Randy
    Archivos de los ejercicios js

---
+++ Practica Javascript/main.js
@@ -0,0 +1,59 @@
+
+//Ejercicio 1
+//Cree un programa que reciba como parámetro un array de números, y
+//verifique la lista de manera que retorne el número mayor.
+
+//const array =[1,2,3,4];
+
+//const max = array.reduce(function(a,b){
+       //return Math.max(a,b);
+    
+  //}
+
+
+//}
+
+
+
+//Ejercicio 2
+//Cree un programa que reciba como parámetro una palabra y verifique si
+//dicha palabra es palíndromo, es decir, la palabra se lee igual //de izquierda a
+//derecha o de derecha a izquierda, o no. Para la resolución de //dicho problema
+//debe utilizar ciclo for . 
+
+//const palabra = ['eye'];
+
+//function polidromo(palabra) {
+  //for (let i = 0; i < palabra.length; i++) {
+    //if (palabra[i]) {
+      
+    //}
+    
+  //}
+  
+//}
+
+//polidromo('');
+
+
+//Ejercicio 3
+
+//Dada la siguiente matriz [ [1,0,0], [0,1,1], [0,1,0] ], haga un programa que
+//cuente e imprima la cantidad de ceros totales en dicha matriz. Debe utilizar
+//ciclos for
+
+
+//const array = [ [1,0,0], [0,1,1], [0,1,0] ];
+//const repetidos = []
+
+
+//const numeros = function (num) {
+  //for (let i = 0; i < array.length; i++) {
+    //repetidos[array0.num] = (repetidos[array1.num] || 0) + 1;
+    
+  //}
+  
+//}
+
+//console.log(repetidos)
+
